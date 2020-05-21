c7c7c4b7dee70e62ab935f2bd5f00f382b3e7f0c
unknown <prakashbahadursingh2@gmail.com>
2020-05-21 08:17:47 -0700

    delete button created and delete function also created with delet request to API to delete movie from the database, on delete action it is directed to movie list page with out the deleted movie

--- client/src/Movies/UpdateMovieForm.js
+++ client/src/Movies/UpdateMovieForm.js
@@ -1,22 +1,36 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, useHistory } from 'react-router-dom';
+import styled from 'styled-components'
+
+const UpdateFormContainer = styled.div`
+text-align: center;
+background: powderblue;
+height: 8rem;
+padding: 2rem;
+`
+const Form = styled.div`
+margin: 2rem;
+width: auto;
+`
+
 
 
 const initialMovie = {
     title: '',
     director: '',
     metaScore: '',
-    stars: '',
+    stars: [],
 }
 
 const UpdateMovieForm = props => {
-    const [movie, setMovie] = useState(initialMovie);
-    const { id } = useParams();
     const { push } = useHistory();
+    const [movie, setMovie] = useState(initialMovie);
+        console.log(movie, '????????????????????????????????????')
+    const { id } = useParams();
 
     useEffect(() => {
-        axios.get(`http://localhost:5000/api/movies/${id}`)
+        axios.get(`http://localhost:5000/api/movies/`)
              .then(res => {
                  console.log(res, 'Movies Data?????????!!!!??!?!?!?!?')
                  setMovie(res.data)
@@ -35,57 +49,60 @@ const UpdateMovieForm = props => {
         e.preventDefault();
         axios.put(`http://localhost:5000/api/movies/${id}`, movie)
             .then(res => {
-                console.log(res, 'Are we putting the movie????!?!?!?!??')
-            push(`/`);
+                console.log(res.data, 'Are we putting the movie????!?!?!?!??')
+            props.setMovieList(res.data);
+            push('/');
+           
         })
             .catch(err => {
                 console.log(err)
             })
+        setMovie(initialMovie);
     };
     
     return (
-        <div>
+        <UpdateFormContainer>
           <h2>Update Movie</h2>
-          <form onSubmit={handleSubmit}>
+          <Form onSubmit={handleSubmit}>
             <label>Title:</label>
                 <input
                 type='text'
                 name='title'
                 onChange={changeHandler}
                 placeholder='Title'
-                value={item.title}
+                value={movie.title}
                 />
             
             <label>Director:</label>
                 <input
-                type='number'
+                type='text'
                 name='director'
                 onChange={changeHandler}
                 placeholder='Director'
-                value={item.director}
+                value={movie.director}
                 />
 
             <label>Metascore:</label>
-            <input
-              type= 'number'
-              name='metaScore'
-              onChange={changeHandler}
-              placeholder='MetaScore'
-              value={item.metaScore}
-            />
+                <input
+                type= 'number'
+                name='metaScore'
+                onChange={changeHandler}
+                placeholder='MetaScore'
+                value={movie.metaScore}
+                />
             <label>Stars:</label>
-            <input
-              type='string'
-              name='stars'
-              onChange={changeHandler}
-              placeholder='Stars'
-              value={item.stars}
-            />
+                <input
+                type='string'
+                name='stars'
+                onChange={changeHandler}
+                placeholder='Stars'
+                value={movie.stars}
+                />
         
-            <button>Update</button>
+            <button style = {{background: 'green', color: 'white'}}>Update</button>
 
-          </form>
-        </div>
+          </Form>
+        </UpdateFormContainer>
       );
 
 
